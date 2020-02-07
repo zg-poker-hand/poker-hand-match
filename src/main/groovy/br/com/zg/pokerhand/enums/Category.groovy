995266382705bf178ac6e5e0ecd1202d57da8a65
Hyago Souza <hyago.souza@zgsolucoes.com.br>
2020-02-07 15:35:12 -0300

    Criando execução mockada no main para testar

--- src/main/groovy/br/com/zg/pokerhand/enums/Category.groovy
+++ src/main/groovy/br/com/zg/pokerhand/enums/Category.groovy
@@ -1,24 +1,57 @@
 package br.com.zg.pokerhand.enums
 
+import br.com.zg.pokerhand.categories.CartaAlta
+import br.com.zg.pokerhand.categories.DoisPares
+import br.com.zg.pokerhand.categories.Flush
+import br.com.zg.pokerhand.categories.FullHouse
+import br.com.zg.pokerhand.categories.Quadra
+import br.com.zg.pokerhand.categories.RoyalFlush
+import br.com.zg.pokerhand.categories.Sequencia
+import br.com.zg.pokerhand.categories.StraightFlush
+import br.com.zg.pokerhand.categories.Trinca
+import br.com.zg.pokerhand.categories.UmPar
+import br.com.zg.pokerhand.interfaces.CategoryStrategy
+
 enum Category {
-	CARTA_ALTA(0),
-	UM_PAR(1),
-	DOIS_PARES(2),
-	TRINCA(3),
-	SEQUENCIA(4),
-	FLUSH(5),
-	FULL_HOUSE(6),
-	QUADRA(7),
-	STRAIGHT_FLUSH(8),
-	ROYAL_FLUSH(9)
+	CARTA_ALTA(0, new CartaAlta()),
+	UM_PAR(1, new UmPar()),
+	DOIS_PARES(2, new DoisPares()),
+	TRINCA(3, new Trinca()),
+	SEQUENCIA(4, new Sequencia()),
+	FLUSH(5, new Flush()),
+	FULL_HOUSE(6, new FullHouse()),
+	QUADRA(7, new Quadra()),
+	STRAIGHT_FLUSH(8, new StraightFlush()),
+	ROYAL_FLUSH(9, new RoyalFlush())
 
 	private Integer value
+	private CategoryStrategy categoryStrategy
 
-	Category(final Integer value) {
+	Category(final Integer value, final CategoryStrategy categoryStrategy) {
 		this.value = value
+		this.categoryStrategy = categoryStrategy
 	}
 
 	Integer getValue() {
 		return value
+	}
+
+	CategoryStrategy getCategoryStrategy() {
+		return categoryStrategy
+	}
+
+	static List<Category> buildHierarchyOfCategories() {
+		return [
+				ROYAL_FLUSH,
+				STRAIGHT_FLUSH,
+				QUADRA,
+				FULL_HOUSE,
+				FLUSH,
+				SEQUENCIA,
+				TRINCA,
+				DOIS_PARES,
+				UM_PAR,
+				CARTA_ALTA,
+		]
 	}
 }
