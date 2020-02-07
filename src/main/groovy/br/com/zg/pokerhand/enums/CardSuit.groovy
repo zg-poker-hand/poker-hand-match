51b0564925e8226d3d5f19700eb95ebc62f0b3ec
Hyago Souza <hyago.souza@zgsolucoes.com.br>
2020-02-07 10:07:44 -0300

    Criando enums

---
+++ src/main/groovy/br/com/zg/pokerhand/enums/CardSuit.groovy
@@ -0,0 +1,9 @@
+package br.com.zg.pokerhand.enums
+
+enum CardSuit {
+	S,
+	H,
+	D,
+	C,
+
+}
---
+++ src/main/groovy/br/com/zg/pokerhand/enums/CardValue.groovy
@@ -0,0 +1,23 @@
+package br.com.zg.pokerhand.enums
+
+enum CardValue {
+	DOIS(2),
+	TRES(3),
+	QUADRO(4),
+	CINCO(5),
+	SEIS(6),
+	SETE(7),
+	OITO(8),
+	NOVE(9),
+	T(10),
+	J(11),
+	Q(12),
+	K(13),
+	A(14),
+
+	private int value
+
+	CardValue(final int value) {
+		this.value = value
+	}
+}
